5fe0e2914dac0a406b0eed9575815482b919a62e
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-02-27 16:19:28 +1100

    initialising relay seems to work, but it doesn't bounce between the bounds

--- src/main/java/ass1/nodes/satellites/RelaySatellite.java
+++ src/main/java/ass1/nodes/satellites/RelaySatellite.java
@@ -3,6 +3,9 @@ package ass1.nodes.satellites;
 import ass1.nodes.NetworkNodeType;
 import static ass1.nodes.NetworkNodeType.RelaySatellite;
 import unsw.utils.Angle;
+import unsw.utils.Orientation;
+import static unsw.utils.Orientation.ANTICLOCKWISE;
+import static unsw.utils.Orientation.CLOCKWISE;
 
 public class RelaySatellite extends Satellite {
     public RelaySatellite(String id, double height, Angle radians) {
@@ -16,7 +19,7 @@ public class RelaySatellite extends Satellite {
             setLinearVelocity(-1.0 * 1500);
             return;
         }
-        
+
         // Satellite in (140°, 190°) or (345°, 360°] or [0°, 140°)
         setLinearVelocity(1500);
     }
@@ -27,8 +30,32 @@ public class RelaySatellite extends Satellite {
 
     @Override
     public void move() {
-        
+        Angle delta = Angle.fromRadians(getLinearVelocity() / getHeight());
 
+        /*
+         * "This means it can briefly exceed the boundary" i.e. set position, then do
+         * the correction.
+         * 
+         * My idea: if not in (140, 190), then just keep going whatever you've been told
+         * to do
+         */
+
+        Angle newPosition = getPosition().add(delta);
+
+        if (!(getPosition().toDegrees() > 140.0 && getPosition().toDegrees() < 190.0)) {
+            // Not in relay region
+            setPosition(newPosition);
+            return;
+        }
+
+        // In edge case, need to reverse
+        if ((orientation() == CLOCKWISE && newPosition.toDegrees() < 140.0)
+                || (orientation() == ANTICLOCKWISE && newPosition.toDegrees() > 190.0)) {
+            reverse();
+        }
+
+        // In relay region, but not edge case
+        setPosition(newPosition);
     }
 
     private void reverse() {
