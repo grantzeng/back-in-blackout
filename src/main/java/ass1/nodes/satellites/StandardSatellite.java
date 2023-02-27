7e96e5ff65962343f094b8e42e3a15fd7567cae6
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-02-27 13:55:05 +1100

    implement standard satellite movement

--- src/main/java/ass1/nodes/satellites/StandardSatellite.java
+++ src/main/java/ass1/nodes/satellites/StandardSatellite.java
@@ -5,20 +5,22 @@ import static ass1.nodes.NetworkNodeType.StandardSatellite;
 import unsw.utils.Angle;
 
 public class StandardSatellite extends Satellite {
-
     public StandardSatellite(String id, double height, Angle degrees) {
         super(id, height, degrees);
         setLinearVelocity(-2500.0);
     }
-    
+
     public NetworkNodeType type() {
         return StandardSatellite;
     }
 
     @Override
     public void move() {
-    
+
         Angle delta = Angle.fromDegrees(getLinearVelocity() / getHeight());
+
+        setPositionDegrees(getPositionDegrees().add(delta));
+
     };
 
 }
