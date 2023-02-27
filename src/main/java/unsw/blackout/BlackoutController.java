5bb9a0c47e21dca6157e5d4f9773faf92f7eb441
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-02-27 13:29:13 +1100

    implement simulate

--- src/main/java/unsw/blackout/BlackoutController.java
+++ src/main/java/unsw/blackout/BlackoutController.java
@@ -19,19 +19,22 @@ import unsw.response.models.EntityInfoResponse;
 import unsw.utils.Angle;
 
 public class BlackoutController {
-
     private Map<String, NetworkNode> nodes = new HashMap<>();
 
     public void createDevice(String deviceId, String type, Angle position) {
         switch (type) {
-            case "HandheldDevice":
-                nodes.put(deviceId, new HandheldDevice(deviceId, position)); break;
-            case "LaptopDevice":
-                nodes.put(deviceId, new LaptopDevice(deviceId, position)); break;
-            case "DesktopDevice":
-                nodes.put(deviceId, new DesktopDevice(deviceId, position)); break;
-            default:
-                System.out.println("No device was added to Blackout"); break;
+        case "HandheldDevice":
+            nodes.put(deviceId, new HandheldDevice(deviceId, position));
+            break;
+        case "LaptopDevice":
+            nodes.put(deviceId, new LaptopDevice(deviceId, position));
+            break;
+        case "DesktopDevice":
+            nodes.put(deviceId, new DesktopDevice(deviceId, position));
+            break;
+        default:
+            System.out.println("No device was added to Blackout");
+            break;
         }
     }
 
@@ -45,17 +48,21 @@ public class BlackoutController {
 
     public void createSatellite(String satelliteId, String type, double height, Angle position) {
         switch (type) {
-            case "StandardSatellite":
-                nodes.put(satelliteId, new StandardSatellite(satelliteId, height, position)); break;
-            case "TeleportingSatellite": 
-                nodes.put(satelliteId, new TeleportingSatellite(satelliteId, height, position)); break;
-            case "RelaySatellite":
-                nodes.put(satelliteId, new RelaySatellite(satelliteId, height, position)); break;
-            default: 
-                System.out.println("No satellite was added to Blackout"); break;
+        case "StandardSatellite":
+            nodes.put(satelliteId, new StandardSatellite(satelliteId, height, position));
+            break;
+        case "TeleportingSatellite":
+            nodes.put(satelliteId, new TeleportingSatellite(satelliteId, height, position));
+            break;
+        case "RelaySatellite":
+            nodes.put(satelliteId, new RelaySatellite(satelliteId, height, position));
+            break;
+        default:
+            System.out.println("No satellite was added to Blackout");
+            break;
         }
     }
-    
+
     /**
      * @pre satelliteId is a valid satellite id
      * @param satelliteId
@@ -65,19 +72,13 @@ public class BlackoutController {
     }
 
     public List<String> listDeviceIds() {
-        return nodes.values()
-                    .stream()
-                    .filter(node -> node instanceof Device)
-                    .map(device -> device.getInfo().getDeviceId())
-                    .collect(Collectors.toList());
+        return nodes.values().stream().filter(node -> node instanceof Device)
+                .map(device -> device.getInfo().getDeviceId()).collect(Collectors.toList());
     }
 
     public List<String> listSatelliteIds() {
-        return nodes.values()
-                    .stream()
-                    .filter(node -> node instanceof Satellite)
-                    .map(satellite -> satellite.getInfo().getDeviceId())
-                    .collect(Collectors.toList());
+        return nodes.values().stream().filter(node -> node instanceof Satellite)
+                .map(satellite -> satellite.getInfo().getDeviceId()).collect(Collectors.toList());
     }
 
     public void addFileToDevice(String deviceId, String filename, String content) {
@@ -89,12 +90,15 @@ public class BlackoutController {
     }
 
     public void simulate() {
-        // TODO: Task 2a)
+
+        for (NetworkNode node : nodes.values()) {
+            node.move();
+        }
     }
 
     /**
-     * Simulate for the specified number of minutes.
-     * You shouldn't need to modify this function.
+     * Simulate for the specified number of minutes. You shouldn't need to modify
+     * this function.
      */
     public void simulate(int numberOfMinutes) {
         for (int i = 0; i < numberOfMinutes; i++) {
