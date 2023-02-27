b6a64109d17ba4a2fdbeedef8851dd5c4cfe58d8
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-02-27 15:13:21 +1100

    fiddling with angle normaliser

--- src/main/java/unsw/utils/AngleNormaliser.java
+++ src/main/java/unsw/utils/AngleNormaliser.java
@@ -1,14 +1,16 @@
 package unsw.utils;
 
 public class AngleNormaliser {
-
     /**
+     * Given an angle in radians, normalises it to be between [0, 2π)
      * 
      * @param radians
      * @return
      */
     public final static Angle normalise(Angle radians) {
-        return degrees;
+        // https://stackoverflow.com/questions/24234609/standard-way-to-normalize-an-angle-to-%CF%80-radians-in-java
+        return Angle.fromRadians(Math.atan2(Math.sin(radians.toRadians()), Math.cos(radians.toRadians())))
+                .add(Angle.fromDegrees(360.0));
     }
 
 }
