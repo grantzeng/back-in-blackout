d2e803534cdfffea964cc9f760aec91f96118217
Grant Zeng <grant.zeng@student.unsw.edu.au>
2023-03-15 17:40:12 +1100

    refactoring network node

--- src/main/java/nodes/NetworkNode.java
+++ src/main/java/nodes/NetworkNode.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import helpers.AngleNormaliser;
+import networking.Connection;
+import networking.File;
 import networking.Server;
 import unsw.response.models.EntityInfoResponse;
 import unsw.response.models.FileInfoResponse;
@@ -15,7 +17,7 @@ public abstract class NetworkNode {
     private String id;
     private Angle position;
     private double height;
-    private List<NetworkNode> visible = new ArrayList<>(); 
+    private Server server;
 
     public enum NodeType {
         DesktopDevice, HandheldDevice, LaptopDevice, RelaySatellite, StandardSatellite, TeleportingSatellite
@@ -25,21 +27,12 @@ public abstract class NetworkNode {
         this.id = id; 
         this.position = position; 
         this.height = height; 
-    }
-        
-    protected abstract List<NodeType> supports();
-    
-    public abstract Server getServer(); 
-    
-    public boolean canSendTo(NetworkNode node) {
-        return supports().contains(node.type()) && MathsHelper.getDistance(height, position, node.getHeight(), node.getPosition()) < range();
+        this.server = new Server(this, 0, 0, 0, 0);
     }
     
-    public abstract NodeType type();
-
-    protected abstract double range();
-
-
+    /*
+        Motion and position
+    */
     public String getId() {
         return id;
     }
@@ -62,24 +55,65 @@ public abstract class NetworkNode {
 
     }
 
-    public void setVisible(List<NetworkNode> visible) {
-        this.visible = visible;
-    }
-
-    public List<NetworkNode> getVisible() {
-        return visible;
-    }
-
     public abstract void move();
 
-    protected abstract Map<String, FileInfoResponse> getServerInfoResponse(); 
+    /*
+        File server behaviour is delegated to server instance.
+    */
+    protected abstract Server setServer();
+    public abstract Server getServer(); 
+    
+    public File getFile(String filename) {
+        server.getFile(filename);
+    }
+    
+    public File addEmptyFile(String filename, int size) {
+        return server.addEmptyFile(filename, size);
+    }
+    
+    public void addUploadConnection(Connection conn) {
+        server.addUploadingConnection(conn);
+    }
+    
+    public void addDownloadConnection(Connection conn) {
+        server.addDownloadingConnection(conn);
+    }
+    
+    public void removeDownloadConnection(Connection conn) {
+        server.removeDownloadConnection(conn);
+    }
+    
+    public removeDownloadConnection(Connection conn, String filename) {
+        server.removeDownloadConnection(conn, filename);
+    }
+    
+    public void removeUploadConnection(Connection conn) {
+        server.removeUploadConnection(conn);
+    }
+    
+    // Try to deal with communicability
+    /*
+    protected abstract List<NodeType> supports();
+    
+    
+    public boolean canSendTo(NetworkNode node) {
+        return supports().contains(node.type()) && MathsHelper.getDistance(height, position, node.getHeight(), node.getPosition()) < range();
+    }
+    
+    public abstract NodeType type();
+
+    protected abstract double range();
+    */
+
+    
+    /*
+        Entity info response
+    */
     
     public EntityInfoResponse getInfo() {
-        return new EntityInfoResponse(id, position, height, type().toString(), getServerInfoResponse());
+        return new EntityInfoResponse(id, position, height, type().toString(), server.getServerInfoResponse());
     }
     
-    public void freeResources() {
-        getServer().freeResources();
-    }
+    
     
 }
