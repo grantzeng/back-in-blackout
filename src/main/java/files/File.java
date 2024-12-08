43779b340572663d2479655b4a5329d1faa8208a
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-12-08 17:09:56 +1100

    stub file class

--- src/main/java/files/File.java
+++ src/main/java/files/File.java
@@ -1,4 +1,5 @@
 /*
+    
     It's clear there should be some kind of file object
     
 
@@ -7,5 +8,61 @@
 import unsw.response.models.FileInfoResponse;
 
 public class File { 
+    
+    public static final int COMPLETE = 1;
+    public static final int PARTIAL = 0; 
+    public static final int TRANSIENT = -1; 
+
+    // public for dev purposes, I hate getters and setters they're just such a shallow abstraction
+    public String fname; 
+    public int size;            // final size, "disk allocation"
+    public int status;
+
+    public String data;         // data.length() will give actual current size
+
+    // We may make other constructors later but for now just...keep it obvious
+
+    public File(String fname, int size, int status, String data) { 
+        this.fname = fname; 
+        this.size = size; 
+        this.status = status;  
+        this.data = content; 
+    }
+
+    /*
+    
+        Interface for accessing file data
+
+        TODO: REMOVE THE GUARD STATEMENTS and replace with something better
+    
+    */
+
+    // read file content
+    // - start at what letter in the data
+    public String read(int fp, int bytes) {
+        if (fp >= size) {
+            System.out.println("Error: fp greater than size")
+            return; 
+        } 
+        
+        if (fp + bytes >= size) {
+            System.out.println("Error: requesting too many bytes starting at fp"); 
+            return; 
+        }
+
+        return data.substring(fp, fp + bytes); 
+
+    }
+
+    public void append(String fragment) { 
+        if (fragment.length() + data.length() > size) {
+            // assert this doesn't happen
+            System.out.println("Error, cannot append exceeds diskspace?"); 
+            return; 
+        }
+
+        data += fragment; 
+    }
+ 
 
 }
